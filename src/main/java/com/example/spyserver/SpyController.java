cb8cc87bd23521a727f194c649c4992c3ed0d70a
MakaronniyMonstr <57591579+MakaronniyMonstr@users.noreply.github.com>
2022-02-06 01:06:49 +0300

    File uploading to server implemented

--- src/main/java/com/example/spyserver/SpyController.java
+++ src/main/java/com/example/spyserver/SpyController.java
@@ -1,5 +1,6 @@
 package com.example.spyserver;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +12,7 @@ import java.nio.file.StandardCopyOption;
 import java.util.Collections;
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping
 public class SpyController {
@@ -26,6 +28,8 @@ public class SpyController {
     @PostMapping("/uploads/{filename}")
     public Map<String, String> uploads(@PathVariable("filename") String filename,
                                        HttpServletRequest request) throws IOException {
+        log.info(filename);
+
         Files.copy(request.getInputStream(), Paths.get(uploadPath + filename), StandardCopyOption.REPLACE_EXISTING);
         return Collections.singletonMap("spy", "ok");
     }
