3f7c74870a55978c7b5883b39afe412c5a1dca38
seregeu <vetrser@gmail.com>
2022-05-20 00:45:19 +0300

    Add base64 decoding

--- src/main/java/com/example/spyserver/SpyController.java
+++ src/main/java/com/example/spyserver/SpyController.java
@@ -5,10 +5,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.Base64;
 import java.util.Collections;
 import java.util.Map;
 
@@ -19,18 +22,14 @@ public class SpyController {
     @Value("${upload.path}")
     private String uploadPath;
 
-    @PostMapping("/spy")
-    public Map<String, String> spy(@RequestBody SpyDTO spyDTO) {
-        System.out.println(spyDTO);
-        return Collections.singletonMap("spy", "ok");
-    }
-
     @PostMapping("/uploads/{filename}")
     public Map<String, String> uploads(@PathVariable("filename") String filename,
+                                       @RequestBody String data,
                                        HttpServletRequest request) throws IOException {
         log.info(filename);
-
-        Files.copy(request.getInputStream(), Paths.get(uploadPath + filename), StandardCopyOption.REPLACE_EXISTING);
+        byte[] decodedBytes = Base64.getDecoder().decode(data);
+        InputStream in = new ByteArrayInputStream(decodedBytes);
+        Files.copy(in, Paths.get(uploadPath + filename), StandardCopyOption.REPLACE_EXISTING);
         return Collections.singletonMap("spy", "ok");
     }
 }
